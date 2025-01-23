be2deeff15afa51aa91d94c6769d9b885da54732
Elia Menoni <eliamenoni@emenoni.eu>
2025-01-23 10:35:34 +0000

    Correzione Warnings

--- RETI_DI_CALCOLATORI/Esercitazioni/Lezione_2/ES03_Callable/Calcolatore.java
+++ RETI_DI_CALCOLATORI/Esercitazioni/Lezione_2/ES03_Callable/Calcolatore.java
@@ -1,19 +1,15 @@
 package RETI_DI_CALCOLATORI.Esercitazioni.Lezione_2.ES03_Callable;
 
-import java.lang.reflect.Array;
-import java.text.Format;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+
 
 public class Calcolatore {
     public static void main(String[] args) {
         ExecutorService pool = Executors.newCachedThreadPool();
         int base = 1;
+        @SuppressWarnings("unchecked")
         Future<Long>[] res = new Future[49];
         for (int i = 2; i <= 50; i++) {
             res[i - 2] = pool.submit(new Power(base, i));
